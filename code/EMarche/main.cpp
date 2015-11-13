e0ba2406e468d108fcb394892c355f4893111d1c
Nicolas Blin <blin5u@fst-ats-303-l05.fst.site.univ-lorraine.fr>
2015-11-13 11:58:17 +0100

    itérateur de la liste de produis

--- code/EMarche/main.cpp
+++ code/EMarche/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <list>
 #include "Produit.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(void)
 {
     /*Produit p;
     p.setQuantite(2);
@@ -13,21 +14,36 @@ int main(int argc, char *argv[])
     cout << "pu " << p.getPrixUnitaire() << endl;
     cout << "depot " << p.getDateDepot() << endl;
     cout << "achat " << p.getDateAchatVente() << endl;*/
-    string nom = "";
-    string categorie = "";
-    float prix = 0;
-    unsigned int quantite = 1;
-    cout << "Mise en vente de votre produit :" << endl <<"Nom : ";
-    cin >> nom;
-    cout << "Categorie :" << endl;
-    cin >> categorie;
-    cout << "Prix/Unite :" << endl;
-    cin >> prix;
-    cout << "Quantite :" << endl;
-    cin >> quantite;
+//    string nom = "";
+//    string categorie = "";
+//    float prix = 0;
+//    unsigned int quantite = 1;
+//    cout << "Mise en vente de votre produit :" << endl <<"Nom : ";
+//    cin >> nom;
+//    cout << "Categorie :" << endl;
+//    cin >> categorie;
+//    cout << "Prix/Unite :" << endl;
+//    cin >> prix;
+//    cout << "Quantite :" << endl;
+//    cin >> quantite;
 
-    Produit prod(nom, categorie, prix, quantite);
-    prod.affiche(cout);
+//    Produit prod(nom, categorie, prix, quantite);
+
+      list<Produit> mylist;
+      Produit p("fdf",string("fdg"),5.6,5);
+      mylist.push_front(p);
+      mylist.push_front(p);
+      mylist.push_front(p);
+      mylist.push_front(p);
+      mylist.push_front(p);
+      mylist.push_front(p);
+      cout << "Liste de produits :\n";
+      for (list<Produit>::iterator it=mylist.begin(); it != mylist.end(); ++it) {
+          it->affiche(cout);
+          cout << ' ';
+          cout << "----------------------------------------\n";
+      }
+      cout << "----------------------------------------\n";
 
     return 0;
 }
